038c6dc2e8d801656270bf8b66f541e51776656e
irawanect <irawanect@gmail.com>
2017-09-28 23:38:32 +0700

    Menambahkan Tugas Baru

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,8 @@ Itu karena kami ingin kelak istri dan anak kami bahagia"""
 
 def tokenize(text):
 	text= text.lower()
-	text = re.sub(r'[^a-z0-9 -]',' ', text, flags = re.IGNORECASE|re.MULTILINE)
+	text = re.sub(r'[^a-z0-9 -]',' ', text, flags = re.IGNORECASE|re.MULTILINE) #karakter selain a-z 0-9 -, diganti dengan spasi kosong#
+	text = re.sub(r'( +)', ' ',text, flags = re.IGNORECASE|re.MULTILINE) #( +) menandakan spasi berlebih akan diganti dengan spasi 1
 	tokens = text.split(" ")
 	return tokens
 
